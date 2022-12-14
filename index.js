78db528f83e87161db2aca0c0e9f28e8a68610f8
felixo2022 <walterbarax@gmail.com>
2022-12-14 04:34:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  /*write  a code that will return true when the word is equal to the reversed word 
+  */
+ /*expect true for 
+ console.log('nan')
+ expect false for 
+ console.log('for')
+ */
+
+  const reversedWord = word.split('').reverse().join('')
+  if (word===reversedWord) {
+    return true
+  }
+  else{return false}
 }
 
 /* 
   Add your pseudocode here
+  -reverse the input of the string
+  -or compare indices without reversing to see if they match
+  -if reversed===word => return true
+  -else ====> retutn false
+  -it is not possible to reverse words in js
+  -it is possible to reverse arrays
+  -it is possible to convert strings to arrays and back to strings again
+  -convert string to array, 
+  -reverse the array
+  -convert the array to string and then compare the results with original
 */
 
 /*
